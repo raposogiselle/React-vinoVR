2d21fa9fb79e7aa1bf264658fb829320063a1bec
Grace Yang <graceyxt.artdesign@gmail.com>
2021-06-29 22:20:14 -0400

    fixed react render issue noticed class components were not exported correctly. Added export class for ex: export class Login extends...

--- vino-vr/client/src/App.js
+++ vino-vr/client/src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import WineBar from './components/Navbar';
+import Home from './components/Home';
 import About from './components/About';
 import Featured from './components/Featured';
 import Footer from './components/Footer';
 import Login from './components/Login';
 import Signup from './components/Signup';
-
 import Favorites from './components/Favorites';
 
+import './App.css';
 
-import Home from './components/Home';
+
 
 
 // import Navbar from 'react-bootstrap/esm/Navbar';
@@ -22,39 +22,16 @@ function App() {
     <Router>
       <div>
         <WineBar />
-       
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/featured" component={Featured} />
           <Route exact path="/myfavorite" component={Favorites} />
-
         <Footer />
       </div>
     </Router>
-
-
-
-
-
-// function App() {
-//   return (
-//     <div>
-//       {/* <Navbar/> */}
-//       <WineBar/>
-//       <About />
-//       <Login />
-//       <Signup />  
-//       <br></br>
-//       <Featured />
-//       <br></br>
-//       <Favorites />
-//     </div>
-    
-// =======
   );
-
 }
    
  
